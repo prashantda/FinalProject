1888ae5a42b793b82b0aa1717e204bd8c6820ff5
Prashant <prashantdarade1697@gmail.com>
2022-09-12 17:27:35 +0530

    abs

--- React/src/Users/SignIn.js
+++ React/src/Users/SignIn.js
@@ -1,4 +1,4 @@
-import { Field, useFormik } from "formik";
+import {  useFormik } from "formik";
 import React, { useContext, useState, useEffect } from "react";
 import * as yup from "yup";
  import {  Link, useNavigate } from "react-router-dom";
@@ -7,7 +7,26 @@ import * as yup from "yup";
 const validationSchema = yup.object({
   username: yup.string().required(),
   password: yup.string().required(),
+//  const validateCustomer=(cdata)=>{
+// const errors={}
+// let pattern1 = /^[a-z0-9._%]+@[a-z0-9.-]+.[a-z]{3}$/
+// let pattern2 = /^[a-zA-Z0-9!@#$%^&*]{6,16}$/
+// if(!cdata.username)
+// errors.username="Email_Id is required"
+//  else if (!pattern1.test(cdata.username))
+//          errors.username = "Use Correct Pattern for Email!"
 
+
+
+// if(!cdata.password)
+// errors.password="password is required"
+// else if (!pattern2.test(cdata.username))
+//         errors.username = "Use Correct Pattern for password!"
+
+
+// return errors
+
+// }
 
   
 });
@@ -112,26 +131,7 @@ function SignIn(props) {
 }
 
 
-//  const validateCustomer=(cdata)=>{
-// const errors={}
-// let pattern1 = /^[a-z0-9._%]+@[a-z0-9.-]+.[a-z]{3}$/
-// let pattern2 = /^[a-zA-Z0-9!@#$%^&*]{6,16}$/
-// if(!cdata.username)
-// errors.username="Email_Id is required"
-//  else if (!pattern1.test(cdata.username))
-//          errors.username = "Use Correct Pattern for Email!"
 
-
-
-// if(!cdata.password)
-// errors.password="password is required"
-// else if (!pattern2.test(cdata.username))
-//         errors.username = "Use Correct Pattern for password!"
-
-
-// return errors
-
-// }
 
 
  export default SignIn
